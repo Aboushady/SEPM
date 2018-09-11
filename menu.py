0ad4053b189ea4e2e8b176132d546bf82419fa12
jontronti <jonas.trondh@gmail.com>
2018-09-11 11:47:20 +0200

    Add files via upload
    fixed valid inputs

--- menu.py
+++ menu.py
@@ -6,7 +6,8 @@ def start_menu():
 
     option = 0
     while(option != '1' and option != '2' and option != '3'):
-        print("welcome\n\n")
+        print('\n')
+        print("           BestGame\n\n")
         print("Please select an option")
         print("1 - VS game")
         print("2 - Tournament")
@@ -17,11 +18,15 @@ def start_menu():
 
     if option == '1':
         mode = 0
-        while (mode <= 0 or mode > 3):
+        while (mode < 1 or mode > 3):
             print('1 - player vs player\n')
             print('2 - player vs ai\n')
             print('3 - ai vs player\n')
-            mode = int(input('choose a VS mode: '))
+            mode = input('choose a VS mode: ')
+            if (mode.isdigit() == False):
+                mode = 0
+            else:
+                mode = int(mode)
             print('\n')
             #Start a game
 
@@ -29,22 +34,32 @@ def start_menu():
 
         players = 0
         while(players < 3 or players > 8):
-            players = int(input('choose amount of players[ 3-8 ]: '))
+
+            players = input('choose amount of players[ 3-8 ]: ')
+            if(players.isdigit() == False):
+                players = 0
+            else:
+                players = int(players)
+
             print('\n')
         remaining = 8 - int(players)
 
         i = 0
         names = []
         while (i < players):
-            names.append(input('Enter name for player %d: ' % (i + 1)))
+            name = input('Enter a 4 character name for player %d: ' % (i + 1))
+            if(len(name) == 4):
+                names.append(name)
+                i += 1
+            else:
+                print('Wrong input. Try again!')
             print('\n')
-            i += 1
 
-        ai = -1
-        while(ai <= -1 or ai > remaining):
-            #ai = input('choose amount of ai[ 0-%d ]:',remaining)
-            ai = int(input('choose amount of ai [ 0 - %d ]: ' % remaining))
-            print('\n')
+        if(remaining >= 1):
+            ai = -1
+            while(ai <= -1 or ai > remaining):
+                ai = int(input('choose amount of ai [ 0 - %d ]: ' % remaining))
+                print('\n')
 
         k = 0
         while(k < remaining):
@@ -52,10 +67,14 @@ def start_menu():
             k += 1
 
         tournament = 0
-        while (tournament != 1 and tournament != 2):
+        while (tournament < 1 or tournament > 2):
             print('1 - round-robin\n')
             print('2 - elimination\n')
-            tournament = int(input('choose a tournament mode: '))
+            tournament = input('choose a tournament mode: ')
+            if (tournament.isdigit() == False):
+                tournament = 0
+            else:
+                tournament = int(tournament)
             print('\n')
 
         #here we should call the different game modes by using the parameter tournament and the names list
