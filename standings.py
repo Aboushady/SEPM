cc82f75831e75de20fd1169016a66bb4ed7765d9
Aboushady <mostafaaboshadee@yahoo.com>
2018-09-12 18:24:45 +0200

    Update standings.py

--- standings.py
+++ standings.py
@@ -2,7 +2,29 @@
 #Or during it, and the type of the tournament is sent as a parameter.
 
 class standings:
-    def showstandings(self, after_tournament):
-        if after_tournament == True
-            for key, value in user_prof_rr.items():
-                print "{first}-----{Second} \n".format(first=key, second=value[1])
+	loser_dict  = {}
+	winner_dict = {}
+    def showstandings_during(self, tournament_type):
+		if tournament_type == 'rr':
+			for key, value in user_prof_rr.items():
+				print "{first}-----{Second} \n".format(first=key, second=value[1])
+		else:
+			for key, value in user_prof_em.items(): #discuss about this approach.
+				if value[2] > 0:
+					loser_dict[key] = value[2]
+				else:
+					winner_dict[key] = value[2]
+				print 'Winners                          Losers \n'
+				print  winner_dict[key]+'             '+loser_dict[key]
+	
+	def showstandings_after(self, tournament_type):
+		if tournament_type == 'rr':
+			self.showstandings_during(tournament_type)
+			sorted_keys  = sorted(user_prof_rr, key=user_prof_rr.__getitem__)
+			sorted_values= sorted(user_prof_rr.value[2])
+			for i in range(1, 4):
+				print i + ':' + sorted_keys[i-1] + '         ' + sorted_values[i-1]
+		else:
+			
+			
+
