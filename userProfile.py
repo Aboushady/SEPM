a2af4cbbddd00a7739ef83be568f94a74d998952
verbonus <mostafaaboshadee@yahoo.com>
2018-09-12 13:43:15 +0200

    User Profiles , and Switching sides functions

--- userProfile.py
+++ userProfile.py
@@ -1,19 +1,33 @@
 import sys
+import random
+import string
 
-user_prof_RR = {}
 
-def getUserInfo_RR(name, points, sideColor_RR):
+user_prof_rr = {}
+user_prof_em = {}
+id_players = {}
+
+#User Info list in Round Robin Tournament.
+def getuserinfo_rr(name, points, sidecolor_rr, listofplayers):
 	id = random_char(4)
-	user_prof_RR[id] = [name, points, sideColor_RR]
-	
+	global user_prof_rr = {id : [name, points, sidecolor_rr]}
+	getlistofplayers(id, listofplayers)
 
+#User Info list in Elimination Tournament.
+def getuserinfo_em(name, sidecolor_em):
+	id = random_char(4)
+	global user_prof_em = {id : [name, sidecolor_em]}
 
-user_prof_EM = {}
-def getUserInfo_EM(name, sideColor_EM):
-	 id = random_char(4)
-	 user_prof_EM[id] = [name, sideColor_EM]
-	
-	
+#Associating each player in the Round Robin Tournament with the list players he didn't play against.
+def getlistofplayers(id, listofplayers):
+	global id_players = {id : listofplayers}
+
+#Switching sides.
+def switchsides(id, switchtocolor, tournamentmode):
+	if tournamentmode == 'rr':
+		user_prof_rr[id][2] = switchtocolor
+	else:
+		user_prof_em[id][1] = switchtocolor
 
 def random_char(y):
-       return ''.join(random.choice(string.ascii_letters) for x in range(y))
+	return ''.join(random.choice(string.ascii_letters) for x in range(y))
