e0177cf7eb25da5adee77f3c803ead13b8b3ded1
verbonus <mostafaaboshadee@yahoo.com>
2018-09-12 14:25:40 +0200

    Get the standing during or after the tournament

---
+++ standings.py
@@ -0,0 +1,8 @@
+#Assuming that the dictionaries are Global, and the flag that says whether this is the standings are after the tournament,
+#Or during it, and the type of the tournament is sent as a parameter.
+
+class standings:
+    def showstandings(self, after_tournament):
+        if after_tournament == True
+            for key, value in user_prof_rr.items():
+                print "{first}-----{Second} \n".format(first=key, second=value[1])
--- userProfile.py
+++ userProfile.py
@@ -2,32 +2,34 @@ import sys
 import random
 import string
 
-
+#These Dictioanry should be initialized in the menu.py.
 user_prof_rr = {}
 user_prof_em = {}
 id_players = {}
 
-#User Info list in Round Robin Tournament.
-def getuserinfo_rr(name, points, sidecolor_rr, listofplayers):
-	id = random_char(4)
-	global user_prof_rr = {id : [name, points, sidecolor_rr]}
-	getlistofplayers(id, listofplayers)
 
-#User Info list in Elimination Tournament.
-def getuserinfo_em(name, sidecolor_em):
-	id = random_char(4)
-	global user_prof_em = {id : [name, sidecolor_em]}
+class userprofiles:
+	#User Info list in Round Robin Tournament.
+	def getuserinfo_rr(self, name, points, sidecolor_rr, listofplayers):
+		id = self.random_char(4)
+		global user_prof_rr = {id : [name, points, sidecolor_rr]}
+		self.getlistofplayers(id, listofplayers)
 
-#Associating each player in the Round Robin Tournament with the list players he didn't play against.
-def getlistofplayers(id, listofplayers):
-	global id_players = {id : listofplayers}
+	#User Info list in Elimination Tournament.
+	def getuserinfo_em(self, name, sidecolor_em):
+		id = self.random_char(4)
+		global user_prof_em = {id : [name, sidecolor_em]}
 
-#Switching sides.
-def switchsides(id, switchtocolor, tournamentmode):
-	if tournamentmode == 'rr':
-		user_prof_rr[id][2] = switchtocolor
-	else:
-		user_prof_em[id][1] = switchtocolor
+	#Associating each player in the Round Robin Tournament with the list players he didn't play against.
+	def getlistofplayers(self, id, listofplayers):
+		global id_players = {id : listofplayers}
 
-def random_char(y):
-	return ''.join(random.choice(string.ascii_letters) for x in range(y))
+	#Switching sides.
+	def switchsides(self, id, switchtocolor, tournamentmode):
+		if tournamentmode == 'rr':
+			user_prof_rr[id][2] = switchtocolor
+		else:
+			user_prof_em[id][1] = switchtocolor
+
+	def random_char(y):
+		return ''.join(random.choice(string.ascii_letters) for x in range(y))
