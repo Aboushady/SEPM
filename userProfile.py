d71c4f5d582d6a4180ce880264ca7e043a23ddfb
Sprogis <chunckey@gmail.com>
2018-09-18 09:30:43 +0200

    refactoring

--- userProfile.py
+++ userProfile.py
@@ -5,45 +5,49 @@ import string
 
 
 class UserProfiles:
-	def __init__(self):
-		# These Dictioanry should be initialized in the menu.py.
-		self.user_prof_rr = {}
-		self.user_prof_em = {}
-		self.id_players = {}
-		self.key_list = ["A", "B", "C", "D", "E", "F", "G", "H"]
-		
-                
-	#User Info list in Round Robin Tournament.
-	def setuserinfo_rr(self, name, points, sidecolor_rr):
-		id_rr = self.random_char()
-                #self.user_prof_rr[id_rr] = [name, points, sidecolor_rr]
-                #self.setlistofplayers(id, listofplayers)
-
-	#User Info list in Elimination Tournament [name, sidecolor, wins, loses].
-	def setuserinfo_em(self, names, sidecolor_em, rounds):
+    def __init__(self):
+        # These Dictioanry should be initialized in the menu.py.
+        # Have one dictionary instead of two called user_profiles
+        self.user_profiles= {}
+        self.id_players = {}
+        self.key_list = ["A", "B", "C", "D", "E", "F", "G", "H"]
+        
+    #User Info list in Elimination Tournament [tournamenttype, list of names, sidecolor, wins, loses].
+    #sidecolor - 1 if black, 0 if white
+    def setuserinfo(self, ttype, names, placement = '', points = 0):
                 self.key_list = self.key_list[0:len(names)]
                 for name in names:
                     id_em = self.random_char()
-                    self.user_prof_em[id_em] = [name, sidecolor_em, rounds]
+                    if(ord(id_em)%2 == 0):
+                        sidecolor = True
+                    else:
+                        sidecolor = False
+                        
+                    if(ttype == 'em'):
+                        self.user_profiles[id_em] = [name, sidecolor, placement]
+                    elif(ttype == 'rr'):      
+                        self.user_profiles[id_em] = [name, sidecolor, placement, points]
+                        
 
-	#Associating each player in the Round Robin Tournament with the list players he didn't play against.
-	def setlistofplayers(self, id, listofplayers):
-		self.id_players[id] = [listofplayers]
+    #Associating each player in the Round Robin Tournament with the list players he didn't play against.
+    def setlistofplayers(self, id, listofplayers):
+        self.id_players[id] = [listofplayers]
 
-	#Switching sides.
-	def switchsides(self, id, tournamentmode):
-		if tournamentmode == 'rr':
-			if self.user_prof_rr[id][2] == 'b':
-				self.user_prof_rr[id][2] = 'w'
-			else:
-				self.user_prof_rr[id][2] = 'b'
-		else:
-			if self.user_prof_em[id][1] == 'b':
-				self.user_prof_em[id][1] = 'w'
-			else:
-				self.user_prof_em[id][1] = 'b'
+    #Switching sides.
+    def switchsides(self, p1, p2):
+                if(self.user_profiles[p1][1] != self.user_profiles[p2][1]):
+                    self.user_profiles[p2][1] = not self.user_profiles[p2][1]
+                    self.user_profiles[p1][1] = not self.user_profiles[p1][1]
+                else:
+                    r = random.random()
+                    if(r < 0.5):
+                        self.user_profiles[p1][1] = True
+                        self.user_profiles[p2][1] = False
+                    else:
+                        self.user_profiles[p1][1] = False
+                        self.user_profiles[p2][1] = True
 
-	def random_char(self):  
+    def random_char(self):  
             r = random.randrange(len(self.key_list))
             key = self.key_list[r]
             del self.key_list[r]
